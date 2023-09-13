70022c6adccd5322d6d03b8e653c3873fb2d9e26
PH_Dat <phungdat0902@gmail.com>
2023-09-13 08:52:22 +0700

    delete be

--- be/src/pdf-translate/pdf-translate.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PdfParserService } from './pdf-translate.service';
-import { PdfParserController } from './pdf-translate.controller';
-
-@Module({
-  controllers: [PdfParserController],
-  providers: [PdfParserService]
-})
-export class PdfTranslateModule { }
--- be/src/user/user.repository.ts
+++
@@ -1,3 +0,0 @@
-import { User } from "./user.entity";
-import { Repository } from 'typeorm';
-export class UserRepository extends Repository<User>{}
